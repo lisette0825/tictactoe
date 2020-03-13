29b5165100efce91d323c10f30143df49f18f339
Aale <aale.liivak@gmail.com>
2020-03-13 13:16:45 +0200

    paksem joon, võitjad

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -20,22 +20,26 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+
 import java.security.KeyStore;
+import java.util.Arrays;
 
 public class Main extends Application {
-    private int RuudustikuLaius= 600;
-    private int RuudustikuKõrgus= 600;
-    private int[][] ruudustik= new int [3][3];
-    boolean esimesekord=true;
+    private int RuudustikuLaius = 600;
+    private int RuudustikuKõrgus = 600;
+    private int[][] ruudustik = new int[3][3];
+    boolean esimesekord = true;
     Canvas canvas;
     GraphicsContext gc;
     private boolean True;
     private boolean False;
+    private int d=0;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         GridPane root = new GridPane();
         Group game = new Group();
@@ -49,23 +53,24 @@ public class Main extends Application {
         HBox hbButtons = new HBox();
         hbButtons.setSpacing(10.0);
         //<>
-        hbButtons.getChildren().addAll(button,button2);
+        hbButtons.getChildren().addAll(button, button2);
         root.add(hbButtons, 0, 0);
         hbButtons.setAlignment(Pos.CENTER);
         //root.add(button2, 0, 1);
         primaryStage.setScene(new Scene(root, 200, 400));
 
-        button.setOnAction(value ->  {
 
-            final Canvas canvas = new Canvas(RuudustikuLaius,RuudustikuKõrgus);
+        button.setOnAction(value -> {
+
+            final Canvas canvas = new Canvas(RuudustikuLaius, RuudustikuKõrgus);
             primaryStage.setScene(new Scene(game, canvas.getWidth(), canvas.getHeight()));
             gc = canvas.getGraphicsContext2D();
             game.getChildren().add(canvas);
             canvas.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
-            for (int i = 1;i<3;i++){
+            for (int i = 1; i < 3; i++) {
 
-                gc.strokeLine(canvas.getWidth()/3*i,0,canvas.getWidth()/3*i,canvas.getHeight());
-                gc.strokeLine(0,canvas.getHeight()/3*i,canvas.getWidth(),canvas.getHeight()/3*i);
+                gc.strokeLine(canvas.getWidth() / 3 * i, 0, canvas.getWidth() / 3 * i, canvas.getHeight());
+                gc.strokeLine(0, canvas.getHeight() / 3 * i, canvas.getWidth(), canvas.getHeight() / 3 * i);
             }
             gc.stroke();
 
@@ -75,57 +80,181 @@ public class Main extends Application {
     }
 
     public void drawRist(int x, int y, int ruudupikkus) {
-        double vahe=0.2;
-        gc.strokeLine((x+vahe)*ruudupikkus,(y+vahe)*ruudupikkus,(x+1-vahe)*ruudupikkus,(y+1-vahe)*ruudupikkus);
-        gc.strokeLine((x+1-vahe)*ruudupikkus,(y+vahe)*ruudupikkus,(x+vahe)*ruudupikkus,(y+1-vahe)*ruudupikkus);
+        double vahe = 0.2;
+        gc.strokeLine((x + vahe) * ruudupikkus, (y + vahe) * ruudupikkus, (x + 1 - vahe) * ruudupikkus, (y + 1 - vahe) * ruudupikkus);
+        gc.strokeLine((x + 1 - vahe) * ruudupikkus, (y + vahe) * ruudupikkus, (x + vahe) * ruudupikkus, (y + 1 - vahe) * ruudupikkus);
 
     }
 
     public void drawRing(int x, int y, int ruudupikkus) {
         double vahe = 0.4;
-        gc.strokeOval((x+vahe/2)*ruudupikkus,(y+vahe/2)*ruudupikkus,ruudupikkus*(1-vahe),ruudupikkus*(1-vahe));
+        gc.strokeOval((x + vahe / 2) * ruudupikkus, (y + vahe / 2) * ruudupikkus, ruudupikkus * (1 - vahe), ruudupikkus * (1 - vahe));
     }
 
     EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() {
         @Override
         public void handle(MouseEvent e) {
-            int x= (int) e.getX();
-            int y= (int) e.getY();
-            int ruutX= RuudustikuLaius/3;
-            int ruutY= RuudustikuKõrgus/3;
+            int x = (int) e.getX();
+            int y = (int) e.getY();
+            int ruutX = RuudustikuLaius / 3;
+            int ruutY = RuudustikuKõrgus / 3;
 
             //<>
+            String[] lõpeta = new String[8];
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
-                    if (x>ruutX*i && x<ruutX*(i+1) && y>ruutY*j && y<ruutY*(j+1)){
+                    if (x > ruutX * i && x < ruutX * (i + 1) && y > ruutY * j && y < ruutY * (j + 1) && d == 0) {
                         System.out.println(i);
                         System.out.println(j);
-                        if(esimesekord){
-                            drawRist(i,j,200);
-                            esimesekord=false;
+                        if (ruudustik[i][j] == 0) {
+                            if (esimesekord) {
+                                ruudustik[i][j] = 1;
+
+                                drawRist(i, j, 200);
+                                esimesekord = false;
+                            } else {
+                                drawRing(i, j, 200);
+                                ruudustik[i][j] = 2;
+                                esimesekord = true;
+                            }
+
                         }
-                        else{
-                            drawRing(i,j,200);
-                            esimesekord=true;
+
+
+                        if (ruudustik[0][0] == 1 && ruudustik[0][1] == 1 && ruudustik[0][2] == 1) {
+                            d = d + 1;
+
+                            gc.setStroke(Color.RED);
+                            gc.setLineWidth(10);
+                            gc.strokeLine(100, 0, 100, 600);
                         }
+                        if (ruudustik[1][0] == 1 && ruudustik[1][1] == 1 && ruudustik[1][2] == 1) {
+                            d = d + 1;
+                            gc.setStroke(Color.RED);
+                            gc.setLineWidth(10);
+                            gc.strokeLine(300, 0, 300, 600);
+                        }
+                        if (ruudustik[2][0] == 1 && ruudustik[2][1] == 1 && ruudustik[2][2] == 1) {
+                            d = d + 1;
+                            gc.setStroke(Color.RED);
+                            gc.setLineWidth(10);
+                            gc.strokeLine(500, 0, 500, 600);
+                        }
+                        if (ruudustik[0][0] == 1 && ruudustik[1][0] == 1 && ruudustik[2][0] == 1) {
+                            d = d + 1;
+                            gc.setStroke(Color.RED);
+                            gc.setLineWidth(10);
+                            gc.strokeLine(0, 100, 600, 100);
+                        }
+                        if (ruudustik[0][1] == 1 && ruudustik[1][1] == 1 && ruudustik[2][1] == 1) {
+                            d = d + 1;
+                            gc.setStroke(Color.RED);
+                            gc.setLineWidth(10);
+                            gc.strokeLine(0, 300, 600, 300);
+                        }
+                        if (ruudustik[0][2] == 1 && ruudustik[1][2] == 1 && ruudustik[2][2] == 1) {
+                            d = d + 1;
+                            gc.setStroke(Color.RED);
+                            gc.setLineWidth(10);
+                            gc.strokeLine(0, 500, 600, 500);
+                        }
+                        if (ruudustik[0][0] == 1 && ruudustik[1][1] == 1 && ruudustik[2][2] == 1) {
+                            d = d + 1;
+                            gc.setStroke(Color.RED);
+                            gc.setLineWidth(10);
+                            gc.strokeLine(0, 0, 600, 600);
+                        }
+                        if (ruudustik[2][0] == 1 && ruudustik[1][1] == 1 && ruudustik[0][2] == 1) {
+                            d = d + 1;
+                            gc.setStroke(Color.RED);
+                            gc.setLineWidth(10);
+                            gc.strokeLine(600, 0, 0, 600);
+                        }
+                        if (ruudustik[0][0] == 2 && ruudustik[0][1] == 2 && ruudustik[0][2] == 2) {
+                            d = d + 2;
+                            gc.setStroke(Color.RED);
+                            gc.setLineWidth(10);
+                            gc.strokeLine(100, 0, 100, 600);
+                        }
+                        if (ruudustik[1][0] == 2 && ruudustik[1][1] == 2 && ruudustik[1][2] == 2) {
+                            d = d + 2;
+                            gc.setStroke(Color.RED);
+                            gc.setLineWidth(10);
+                            gc.strokeLine(300, 0, 300, 600);
+                        }
+                        if (ruudustik[2][0] == 2 && ruudustik[2][1] == 2 && ruudustik[2][2] == 2) {
+                            d = d + 2;
+                            gc.setStroke(Color.RED);
+                            gc.setLineWidth(10);
+                            gc.strokeLine(500, 0, 500, 600);
+                        }
+                        if (ruudustik[0][0] == 2 && ruudustik[1][0] == 2 && ruudustik[2][0] == 2) {
+                            d = d + 2;
+                            gc.setStroke(Color.RED);
+                            gc.setLineWidth(10);
+                            gc.strokeLine(0, 100, 600, 100);
+                        }
+                        if (ruudustik[0][1] == 2 && ruudustik[1][1] == 2 && ruudustik[2][1] == 2) {
+                            d = d + 2;
+                            gc.setStroke(Color.RED);
+                            gc.setLineWidth(10);
+                            gc.strokeLine(0, 300, 600, 300);
+                        }
+                        if (ruudustik[0][2] == 2 && ruudustik[1][2] == 2 && ruudustik[2][2] == 2) {
+                            d = d + 2;
+                            gc.setStroke(Color.RED);
+                            gc.setLineWidth(10);
+                            gc.strokeLine(0, 500, 600, 500);
+                        }
+                        if (ruudustik[0][0] == 2 && ruudustik[1][1] == 2 && ruudustik[2][2] == 2) {
+                            d = d + 2;
+                            gc.setStroke(Color.RED);
+                            gc.setLineWidth(10);
+                            gc.strokeLine(0, 0, 600, 600);
+                        }
+                        if (ruudustik[2][0] == 2 && ruudustik[1][1] == 2 && ruudustik[0][2] == 2) {
+                            d = d + 2;
+                            gc.setLineWidth(10);
+                            gc.setStroke(Color.RED);
+                            gc.strokeLine(600, 0, 0, 600);
+                        }
+
 
                     }
 
                 }
+
             }
 
 
+            if (d == 1) {
+                System.out.println("ESIMENE VÕITIS");
+
+
+            }
+            if (d == 2) {
+                System.out.println("TEINE ON NEEGER");
+            } else {
+                System.out.println("Kumbki ei võitnud");
+            }
+
 
         }
-    };
+    }
+
+
+
+
+            ;
 //Adding event Filter
 
     public static void main(String[] args) {
         launch(args);
 
     }
-    public Rectangle ruut(){
-        Rectangle border= new Rectangle(200,200);
+
+    public Rectangle ruut() {
+        Rectangle border = new Rectangle(200, 200);
         border.setFill(null);
         border.setStroke(Color.BLACK);
         //setAlignment(Pos.CENTER);
